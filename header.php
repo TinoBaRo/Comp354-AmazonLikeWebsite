2c2084456d0b28887df8210247e06041ef76388b
Celestino <tinoteross@gmail.com>
2019-10-23 16:37:02 -0400

    Logo Added to Navigation bar

--- header.php
+++ header.php
@@ -21,7 +21,9 @@
 	<body>
 
 		<nav class="navbar navbar-expand-lg navbar-light bg-light">
-		  <a class="navbar-brand" href="#">Navbar</a>
+		  <a class="navbar-brand" href="#">
+		  	<img src="logo354TheStars.png" height="50" width="100"> <!-- Logo Picture -->
+		  </a>
 		  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
 		    <span class="navbar-toggler-icon"></span>
 		  </button>
@@ -47,10 +49,7 @@
 		          		<a class="dropdown-item" href="#">Something else here</a>
 		        	</div>
 		      	</li>
-		      	
-		      	<li class="nav-item">
-		        	<a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
-		      	</li>
+		      	  	
 		    </ul>
 		    
 		    <form class="form-inline my-2 my-lg-0">
