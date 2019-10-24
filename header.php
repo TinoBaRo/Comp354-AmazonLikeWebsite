69a646f388c4af0033166f6fe96788aae819eba5
Celestino <tinoteross@gmail.com>
2019-10-23 22:27:47 -0400

    Added a Gallery where all the products will be desplayed on the home page

--- header.php
+++ header.php
@@ -8,7 +8,9 @@
 
 		<!-- Bootstrap css ***needs to go before other css files*** -->
 		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-		<link rel="stylesheet" type="text/css" href="Style.css">
+
+		<link rel="stylesheet" type="text/css" href="generalStyle.css">
+		<link rel="stylesheet" type="text/css" href="albumLayout.css">
 		
 		<meta charset="utf-8">
 
@@ -19,36 +21,38 @@
 
 
 	<body>
-
 		<nav class="navbar navbar-expand-lg navbar-light bg-light">
-		  <a class="navbar-brand" href="#">
-		  	<img src="logo354TheStars.png" height="50" width="100"> <!-- Logo Picture -->
-		  </a>
-		  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+		  	<a class="navbar-brand" href="#">
+		  		<img src="logo354TheStars.png" height="50" width="100"> <!-- Logo Picture -->
+		  	</a>
+		  	<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
 		    <span class="navbar-toggler-icon"></span>
-		  </button>
+		  	</button>
 
 
 		<div class="collapse navbar-collapse" id="navbarSupportedContent">
 		    <ul class="navbar-nav mr-auto">
+
 		      	<li class="nav-item active">
 		        	<a class="nav-link" href="homePage.php">Home <span class="sr-only">(current)</span></a>
 		      	</li>
-		      	<li class="nav-item">
+
+		      	<li class="nav-item active">
 		       	 	<a class="nav-link" href="loginPage.php">Login</a>
 		      	</li>
 		      	
-		      	<li class="nav-item dropdown">
-		        	<a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"> Dropdown
-		        	</a>
-		        	<div class="dropdown-menu" aria-labelledby="navbarDropdown">
-		          		<a class="dropdown-item" href="#">Action</a>
-		          		<a class="dropdown-item" href="#">Another action</a>
+		      	
+			    <li class="nav-item dropdown">
+			        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">
+			        Categories
+			        </a>
 
-		         	<div class="dropdown-divider"></div>
-		          		<a class="dropdown-item" href="#">Something else here</a>
-		        	</div>
-		      	</li>
+			        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+			          <a class="dropdown-item" href="#">Action</a>
+			          <a class="dropdown-item" href="#">Another action</a>
+			        </div>
+			    </li>
+
 		      	  	
 		    </ul>
 		    
@@ -56,7 +60,9 @@
 		      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
 		      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
 		    </form>
+
 		</div>
 
 		</nav>
+		
         
--- homePage.php
+++ homePage.php
@@ -9,6 +9,162 @@
 		<h3>Home Page</h3>
 	</p>
 
+	<main role="main">
+
+	<div class="album py-5 bg-light">
+	    <div class="container">
+
+	      <div class="row">
+	        <div class="col-md-4">
+	          <div class="card mb-4 shadow-sm">
+	            <img src="dimeShirt.jpg" height="300"> <!--IMAGE item 1 -->
+	            <div class="card-body">
+	              <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+	              <div class="d-flex justify-content-between align-items-center">
+	                <div class="btn-group">
+	                  <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
+	                  <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button>
+	                </div>
+	                <small class="text-muted">9 mins</small>
+	              </div>
+	            </div>
+	          </div>
+	        </div>
+
+	        <div class="col-md-4">
+	          <div class="card mb-4 shadow-sm">
+	            <img src="dimeShirt.jpg" height="300"> <!--IMAGE item 2 -->
+	            <div class="card-body">
+	              <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+	              <div class="d-flex justify-content-between align-items-center">
+	                <div class="btn-group">
+	                  <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
+	                  <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button>
+	                </div>
+	                <small class="text-muted">9 mins</small>
+	              </div>
+	            </div>
+	          </div>
+	        </div>
+
+	        <div class="col-md-4">
+	          <div class="card mb-4 shadow-sm">
+	            <img src="dimeShirt.jpg" height="300"> <!--IMAGE item 3 -->
+	            <div class="card-body">
+	              <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+	              <div class="d-flex justify-content-between align-items-center">
+	                <div class="btn-group">
+	                  <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
+	                  <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button>
+	                </div>
+	                <small class="text-muted">9 mins</small>
+	              </div>
+	            </div>
+	          </div>
+	        </div>
+
+	        <div class="col-md-4">
+	          <div class="card mb-4 shadow-sm">
+	            <img src="dimeShirt.jpg" height="300"> <!--IMAGE item 4 -->
+	            <div class="card-body">
+	              <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+	              <div class="d-flex justify-content-between align-items-center">
+	                <div class="btn-group">
+	                  <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
+	                  <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button>
+	                </div>
+	                <small class="text-muted">9 mins</small>
+	              </div>
+	            </div>
+	          </div>
+	        </div>
+
+	        <div class="col-md-4">
+	          <div class="card mb-4 shadow-sm">
+	            <img src="dimeShirt.jpg" height="300"> <!--IMAGE item 5 -->
+	            <div class="card-body">
+	              <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+	              <div class="d-flex justify-content-between align-items-center">
+	                <div class="btn-group">
+	                  <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
+	                  <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button>
+	                </div>
+	                <small class="text-muted">9 mins</small>
+	              </div>
+	            </div>
+	          </div>
+	        </div>
+
+	        <div class="col-md-4">
+	          <div class="card mb-4 shadow-sm">
+	            <img src="dimeShirt.jpg" height="300"> <!--IMAGE item 6 -->
+	            <div class="card-body">
+	              <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+	              <div class="d-flex justify-content-between align-items-center">
+	                <div class="btn-group">
+	                  <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
+	                  <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button>
+	                </div>
+	                <small class="text-muted">9 mins</small>
+	              </div>
+	            </div>
+	          </div>
+	        </div>
+
+	        <div class="col-md-4">
+	          <div class="card mb-4 shadow-sm">
+	            <img src="dimeShirt.jpg" height="300"> <!--IMAGE item 7 -->
+	            <div class="card-body">
+	              <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+	              <div class="d-flex justify-content-between align-items-center">
+	                <div class="btn-group">
+	                  <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
+	                  <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button>
+	                </div>
+	                <small class="text-muted">9 mins</small>
+	              </div>
+	            </div>
+	          </div>
+	        </div>
+
+	        <div class="col-md-4">
+	          <div class="card mb-4 shadow-sm">
+	            <img src="dimeShirt.jpg" height="300"> <!--IMAGE item 8 -->
+	            <div class="card-body">
+	              <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+	              <div class="d-flex justify-content-between align-items-center">
+	                <div class="btn-group">
+	                  <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
+	                  <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button>
+	                </div>
+	                <small class="text-muted">9 mins</small>
+	              </div>
+	            </div>
+	          </div>
+	        </div>
+
+	        <div class="col-md-4">
+	          <div class="card mb-4 shadow-sm">
+	            <img src="dimeShirt.jpg" height="300"> <!--IMAGE item 9 -->
+	            <div class="card-body">
+	              <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+	              <div class="d-flex justify-content-between align-items-center">
+	                <div class="btn-group">
+	                  <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
+	                  <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button>
+	                </div>
+	                <small class="text-muted">9 mins</small>
+	              </div>
+	            </div>
+	          </div>
+	        </div>
+	      </div>
+	    </div>
+	</div>
+
+	</main>
+
+
 
 <?php
     require("footer.php");
