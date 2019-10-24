d82514350e9abd85ee80a5f8c68d53595a8d76e5
hbj94 <56208123+hbj94@users.noreply.github.com>
2019-10-23 20:28:21 -0400

    Update header.php
    Adapted the logo on the page
    created drop down menu that redirects to login page
    
    I will update file later to change dropdown from login to Categories
    Will also change the color of it to match the rest

--- header.php
+++ header.php
@@ -1,62 +1,64 @@
 <!DOCTYPE html>
 
 <html>
-	
+    
 
-	<head>
-		<title>354 The Stars</title>
+    <head>
+        <title>354 The Stars</title>
 
-		<!-- Bootstrap css ***needs to go before other css files*** -->
-		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-		<link rel="stylesheet" type="text/css" href="Style.css">
-		
-		<meta charset="utf-8">
-
-		<script type="text/javascript" src="someJavaScriptFile.js"> 
-		</script>
-
-	</head>
-
-
-	<body>
-
-		<nav class="navbar navbar-expand-lg navbar-light bg-light">
-		  <a class="navbar-brand" href="#">
-		  	<img src="logo354TheStars.png" height="50" width="100"> <!-- Logo Picture -->
-		  </a>
-		  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-		    <span class="navbar-toggler-icon"></span>
-		  </button>
-
-
-		<div class="collapse navbar-collapse" id="navbarSupportedContent">
-		    <ul class="navbar-nav mr-auto">
-		      	<li class="nav-item active">
-		        	<a class="nav-link" href="homePage.php">Home <span class="sr-only">(current)</span></a>
-		      	</li>
-		      	<li class="nav-item">
-		       	 	<a class="nav-link" href="loginPage.php">Login</a>
-		      	</li>
-		      	
-		      	<li class="nav-item dropdown">
-		        	<a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"> Dropdown
-		        	</a>
-		        	<div class="dropdown-menu" aria-labelledby="navbarDropdown">
-		          		<a class="dropdown-item" href="#">Action</a>
-		          		<a class="dropdown-item" href="#">Another action</a>
-
-		         	<div class="dropdown-divider"></div>
-		          		<a class="dropdown-item" href="#">Something else here</a>
-		        	</div>
-		      	</li>
-		      	  	
-		    </ul>
-		    
-		    <form class="form-inline my-2 my-lg-0">
-		      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-		      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-		    </form>
-		</div>
-
-		</nav>
+        <!-- Bootstrap css ***needs to go before other css files*** -->
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+        <link rel="stylesheet" type="text/css" href="Style.css">
         
+        <meta charset="utf-8">
+
+        <script type="text/javascript" src="someJavaScriptFile.js">
+        </script>
+
+    </head>
+
+
+    <body>
+
+        <nav class="nav navbar-toggleable-md navbar-expand-lg navbar-light bg-light">
+<button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+   <span class="navbar-toggler-icon"></span>
+ </button>
+        
+<a class="navbar-brand" href="homePage.php"><img src="logo354TheStars.png" height="50" width="100"> <!-- Logo Picture --></a>
+        <div class="collapse navbar-collapse" id="navbarNavDropdown">
+            <ul class="navbar-nav">
+                  <li class="nav-item active">
+                    <a class="nav-link" href="homePage.php">Home <span  class="sr-only">(current)</span></a>
+                  </li>
+                  <li class="nav-item">
+                        <a class="nav-link" href="loginPage.php">Login</a>
+                  </li>
+                   
+                 </div>
+                    <li class="nav-link dropdown">
+                    <a class="nav-link dropdown-toggle" href="loginPage.php" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"> Login ?
+                    </a>
+                        <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                          <a class="dropdown-item" href="loginPage.php">SignUp</a>
+                          <a class="dropdown-item" href="loginPage.php">SignIn</a>
+                        <a class="dropdown-item" href="#">Something else here</a>
+                     </div>
+                  </li>
+                  
+              
+            </ul>
+            
+            <form class="form-inline my-2 my-lg-0">
+              <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+              <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+            </form>
+        </div>
+
+        </nav>
+        
+<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+</body>
+</html>
