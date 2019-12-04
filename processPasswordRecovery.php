f91be9a14a9a85999d7963a7220291e81627be5e
Saffia <saffia.yniro@hotmail.com>
2019-12-04 00:37:51 -0500

    update logo size

--- processPasswordRecovery.php
+++ processPasswordRecovery.php
@@ -84,8 +84,8 @@
 
 <div class="card">
     <div class="card-block">
-    	<div class="mx-auto" style="width: 600px;">
-        <img class="card-img-top" src="images\logo.png" alt="Card image cap">
+    	<div class="mx-auto" style="width: 400px;">
+        <img class="card-img-top" src="images\logo.png" alt="Card image cap"></img>
 
 <?php  
 
