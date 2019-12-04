107528024842d2711ecbb948fd4432b05d5e6322
Celestino <tinoteross@gmail.com>
2019-12-04 00:08:51 -0500

    Added logo to recovery pass

--- processPasswordRecovery.php
+++ processPasswordRecovery.php
@@ -85,7 +85,7 @@
 <div class="card">
     <div class="card-block">
     	<div class="mx-auto" style="width: 600px;">
-        <img class="card-img-top" src="logo354TheStars.png" alt="Card image cap">
+        <img class="card-img-top" src="images\logo.png" alt="Card image cap">
 
 <?php  
 
